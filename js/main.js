b8121a78bb02518d54a6ef7a9196b70d389e1e68
Caro Viggiano <caroviggiano05@gmail.com>
2023-06-19 21:07:27 -0300

    ya basta de JS

--- js/main.js
+++ js/main.js
@@ -1,113 +1,3 @@
-/*
-function calcularMonto(lts, combus) {
-  let monto = 0;
-  if (combus === 1) {
-    monto = lts * vibraniums;
-  }
-
-  if (combus === 2) {
-    monto = lts * paladiumi;
-  }
-
-  if (combus === 3) {
-    monto = lts * cryptonid;
-  }
-
-  if (combus === 4) {
-    monto = lts * alamantiumud;
-  }
-
-  if (combus === 5) {
-    monto = lts * galliongo;
-  }
-  return monto;
-}
-
-function pagoEfectivo() {
-  let efect = confirm(
-    "Desea pagar con efectivo? Seleccione ACEPTAR para pagar con efectivo."
-  );
-  return efect;
-}
-
-function pagoTarjeta() {
-  let tarje = confirm(
-    "Desea pagar con targeta? Si ya selecciono el metodo por efectivo, oprima CANCELAR."
-  );
-  return tarje;
-}
-
-let operacion = ingresarOpcion();
-
-if (!isNaN(operacion)) {
-  if (operacion != 2) {
-    let combustible = ingresarCombustible();
-    let litros = ingresarLitros();
-    let monto = calcularMonto(litros, combustible);
-
-    alert("Su monto total a pagar es de: " + monto);
-
-    let efectivo = pagoEfectivo();
-    let tarjeta = pagoTarjeta();
-
-    if (efectivo) {
-      let saldo = Number(prompt("Ingrese su saldo"));
-      if (saldo > monto) {
-        alert("Pago realizado con exito!");
-      } else if (saldo < monto) {
-        alert(
-          "Saldo insuficiente! Debera pagar con targeta de credito o debito. El pago se realizara de forma instantanea."
-        );
-        tarjeta = true;
-      }
-    }
-
-    if (tarjeta) {
-      for (i = 0; i < 3; i++) {
-        let contrasenia = Number(
-          prompt("Ingrese su DNI que tenga registrado en las tarjetas:")
-        );
-        if (contrasenia === 12345) {
-          let targetadoc = confirm(
-            "Oprima ACEPTAR para pagar con debito u oprima CANCELAR para pagar con credito"
-          );
-          if (targetadoc) {
-            let saldo = Number(
-              prompt("Ingrese el saldo de la tarjeta de debito")
-            );
-            if (saldo > monto) {
-              alert("Pago realizado con exito!");
-              break;
-            } else if (saldo < monto) {
-              alert(
-                "Saldo insuficiente! Debera pagar con targeta de credito. El pago se realizara de forma instantanea. Gracias por confiar en nosotros!"
-              );
-              targetadoc = false;
-              break;
-            }
-          } else if (!targetadoc) {
-            alert("El pago se realizara de forma instantanea.");
-            alert("Pago realizado con exito. Gracias por confiar en nosotros!");
-            break;
-          }
-        } else {
-          alert(
-            "Contrasenia incorrecta. Le quedan: " + (2 - [i]) + " intentos"
-          );
-        }
-      }
-    }
-  } else {
-    alert("Buen viaje y esperamos tenerlo por aqui nuevamente!");
-  }
-} else {
-  alert("INGRESE UN NUMERO VALIDO. OPERACION FINALIZADA.");
-}
-
-alert("Buen viaje y esperamos tenerlo por aqui nuevamente!");
-
-*/
-
 const alfajores = 250
 const caramelos = 10
 const gomitas = 100
@@ -252,6 +142,15 @@ if (opcionSeleccionada === 1) {
   }
 
   let montoCargaSUBE = cargarSUBE();
-  alert("Es correcto el monto de carga de SUBE: ", montoCargaSUBE " ?");
+  alert("Es correcto el monto de carga de SUBE: ", montoCargaSUBE , " ? ");
 }
 
+function pago(){
+  let pago = prompt("¿Que metodo de pago prefiere? \n1. Efectivo \n2. Mercado Pago \n3. Crédito/Débito")
+} if  (pago === 1) {
+  alert ("Pagale al vendedor, rata. Muchas gracias por su compra!")
+} else if (pago === 2 ){
+  alert ("Pidale al vendedor el QR. Muchas gracias por su compra!")
+} else if (pago === 3 ){
+  alert ("Pasele la tarjeta al vendedor. Muchas gracias por su compra!")
+}
